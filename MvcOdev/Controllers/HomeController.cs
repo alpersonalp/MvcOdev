c28c5fba751835e73fe5fce902c49cbadd23bf8c
Alper <alper.sonalp@gmail.com>
2022-05-31 15:19:35 +0300

    chng

--- MvcOdev/Controllers/HomeController.cs
+++ MvcOdev/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace MvcOdev.Controllers
 
             List<Haberler> hbrList = new List<Haberler>();
             hbrList.Add(new Haberler {haberbaslik= "Haber 1 :",kisatext = " C# Prog. dilini öğrendik.Artık c# dilini biliyoruz.",tarih="19.05.22" });
-            hbrList.Add(new Haberler {haberbaslik= "Haber 2 :",kisatext = "Haber 2 : Sql Server Öğrendik.Hepimiz birer DBA'yiz..",tarih="20.05.22" });
+            hbrList.Add(new Haberler {haberbaslik= "Haber 2 :",kisatext = " Sql Server Öğrendik.Hepimiz birer DBA'yiz..",tarih="20.05.22" });
             hbrList.Add(new Haberler {haberbaslik= "Haber 3 :",kisatext = " EF Core Örendik. Artık veritabanı ile sqli bağlıyoruz.",tarih="21.05.22" });
             hbrList.Add(new Haberler {haberbaslik= "Haber 4 :",kisatext = " Ve nihayet webteyiz. Hatta Asp.Net Core öğreneceğiz.",tarih="29.05.22" });
             hbrList.Add(new Haberler {haberbaslik= "Haber 5 :",kisatext = " Hoca dapperda anlatacakmış.",tarih="30.05.22" });
