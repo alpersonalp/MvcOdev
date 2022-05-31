e4d4584f3973007c94f1f44a484a1809e7de72ea
Alper <alper.sonalp@gmail.com>
2022-05-31 15:19:00 +0300

    Add project files.

---
+++ MvcOdev/Models/Haberler.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcOdev.Models
+{
+    public class Haberler
+    {
+        public string? haberbaslik { get; set; }
+        public string? kisatext { get; set; }
+
+        public string? tarih { get; set; }
+    }
+}
